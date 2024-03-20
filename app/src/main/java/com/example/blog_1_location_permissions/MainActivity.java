da47c28846bcc5cf810bf7a175a94be83650e878
metacreature <MY_NAME@example.com>
2024-03-20 22:54:21 +0100

    only one permission

--- app/src/main/java/com/example/blog_1_location_permissions/MainActivity.java
+++ app/src/main/java/com/example/blog_1_location_permissions/MainActivity.java
@@ -9,7 +9,6 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private int LOCATION_PERMISSION_CODE = 1;
-    private int BACKGROUND_LOCATION_PERMISSION_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkPermission() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            // Fine Location permission is granted
-            // Check if current android version >= 11, if >= 11 check for Background Location permission
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    // Background Location Permission is granted so do your work here
-                } else {
-                    // Ask for Background Location Permission
-                    askPermissionForBackgroundUsage();
-                }
-            }
-        } else {
-            // Fine Location Permission is not granted so ask for permission
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             askForLocationPermission();
         }
     }
@@ -79,58 +65,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void askPermissionForBackgroundUsage() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
-            new AlertDialog.Builder(this)
-                    .setTitle("Permission Needed!")
-                    .setMessage("Background Location Permission Needed!, tap \"Allow all time in the next screen\"")
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MainActivity.this,
-                                    new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, BACKGROUND_LOCATION_PERMISSION_CODE);
-                        }
-                    })
-                    .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // User declined for Background Location Permission.
-                        }
-                    })
-                    .create().show();
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, BACKGROUND_LOCATION_PERMISSION_CODE);
-        }
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         if (requestCode == LOCATION_PERMISSION_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // User granted location permission
-                // Now check if android version >= 11, if >= 11 check for Background Location Permission
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        // Background Location Permission is granted so do your work here
-                    } else {
-                        // Ask for Background Location Permission
-                        askPermissionForBackgroundUsage();
-                    }
-                }
+                // granted
             } else {
                 // User denied location permission
             }
-        } else if (requestCode == BACKGROUND_LOCATION_PERMISSION_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // User granted for Background Location Permission.
-            } else {
-                // User declined for Background Location Permission.
-            }
         }
-
     }
-
 }
